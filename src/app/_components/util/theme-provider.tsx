aec5afb29ad51bde48a0b739e8293f3e07272d62
Federico Grandi <fgrandi30@gmail.com>
2024-12-26 20:05:42 +0100

    Prevent theme flash on load

--- src/app/_components/util/theme-provider.tsx
+++ src/app/_components/util/theme-provider.tsx
@@ -7,5 +7,15 @@ export function ThemeProvider({
   children,
   ...props
 }: React.ComponentProps<typeof NextThemesProvider>) {
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null; // Prevent theme flash on initial load
+  }
+
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
